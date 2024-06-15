fe22490d315e5f4d495fe61eb4e382259d3f1ba6
JackLiu999 <60761342+JackLiu999@users.noreply.github.com>
2024-06-15 00:35:48 -0400

    second step

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [formData, setFormData] = useState({
+    shipmentPickup: '',
+    pickupZip: '',
+    pickupType: '',
+    deliveryZip: '',
+    deliveryType: '',
+    quantity: '',
+    packagingType:'',
+    length: '',
+    width:'',
+    height:'',
+    totalWeight:'',
+    freightClass:'',
+    description:'',
+    additionalService:'',
+  })
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Form submitted:', formData);
+    // You can also add code to send formData to a server here
+  };
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="shipmentPickup">Shipment Pickup Date:</label>
+        <input
+          type="date"
+          id="shipmengPickup"
+          name="nshipmengPickup"
+          value={formData.shipmentPickup}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="pickupZip">Pickup Zip:</label>
+        <input
+          type="text"
+          id="text"
+          name="text"
+          value={formData.pickupZip}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="pickupType">Pickup Type:</label>
+        <input
+          id="pickupType"
+          name="pickupType"
+          value={formData.pickupType}
+          onChange={handleChange}
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
   );
 }
 
